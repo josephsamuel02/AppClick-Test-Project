ba30c992954f3860577b8b0dc43e65bad7d08694
josephsamuel02 <mrsamd02@gmail.com>
2022-02-10 11:55:28 +0100

    .

--- client/src/App.js
+++ client/src/App.js
@@ -9,7 +9,7 @@ import {
 
 import Index from "./components/UserId/Index";
 import Home from "./components/Home/Home";
-import FormTemplates from "./components/FormTemplates/FormTemplates";
+import FormTemplates from "./components/AppInputs/FormTemplates";
 import Preview from "./components/Preview/Preview";
 function App() {
     const theuser = useSelector((state) => state.RegisterUser.username);
